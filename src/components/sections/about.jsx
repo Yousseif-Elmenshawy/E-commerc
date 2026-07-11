599a121b5705e03c32315a2bc4b8d22344d7bb06
Yousseif Elshenawy <yussefelshenawy@gmail.com>
2026-07-11 15:59:28 +0300

    fix the about section

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -4,10 +4,10 @@ export default function About(){
     return(
         <>
             <section id="About" className="AB-section container">
-                <div className="box1 w-50">
-                    <h2>ABOUT US</h2>
-                    <p className="box-p w-50">Brutal is a digital studio dedicated to crafting unforgettable, high-performance footwear that challenges conventional design. We prioritize "Bold by Design" aesthetics, blending uncompromising durability with raw, unpretentious style for the modern individual. Our mission is to engineer products that stand out, ensuring every pair delivers both superior functionality.</p>
-                </div>
+                    <div className="box1 w-50">
+                        <h2>ABOUT US</h2>
+                        <p className="box-p">Brutal is a digital studio dedicated to crafting unforgettable, high-performance footwear that challenges conventional design. We prioritize "Bold by Design" aesthetics, blending uncompromising durability with raw, unpretentious style for the modern individual. Our mission is to engineer products that stand out, ensuring every pair delivers both superior functionality.</p>
+                    </div>
                 <div className="box2">
                     <div className="img img-fluid">
                         <img src="#" alt="#" />
