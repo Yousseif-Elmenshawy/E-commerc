2c291832d52d256fada6a437663e89093abff489
Yousseif Elshenawy <yussefelshenawy@gmail.com>
2026-07-13 18:24:58 +0300

    fix layout again

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -4,7 +4,7 @@ export default function About(){
     return(
         <>
             <section id="About" className="AB-section container">
-                    <div className="box1 w-50">
+                    <div className="box1">
                         <h2>ABOUT US</h2>
                         <p className="box-p">Brutal is a digital studio dedicated to crafting unforgettable, high-performance footwear that challenges conventional design. We prioritize "Bold by Design" aesthetics, blending uncompromising durability with raw, unpretentious style for the modern individual. Our mission is to engineer products that stand out, ensuring every pair delivers both superior functionality.</p>
                     </div>
